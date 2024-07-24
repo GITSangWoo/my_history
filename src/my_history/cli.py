df26054aa753fc476f50ffc81ebc05c9f9220e55
GITSangWoo <dogcenta@gmail.com>
2024-07-24 17:03:51 +0900

    what

--- src/my_history/cli.py
+++ src/my_history/cli.py
@@ -1,14 +1,26 @@
 import pandas as pd
 import sys 
 
-def cnt():
-    command = (sys.argv[1])
-    df = pd.read_parquet("~/tmp/history.parquet")
+def cnt(q):
+    df = read_parquet()
+    df = pd.read_parquet("~/tmp/history.parquet") 
     df['dt'] = df['dt'].str.replace('^', '')
     df['cmd'] = df['cmd'].str.replace('^', '')
     df['cnt'] = df['cnt'].str.replace('^', '')
     df['cnt'] = df['cnt'].astype(int)
-    fdf = df[df['cmd'].str.contains(command)]
+    fdf = df[df['cmd'].str.contains(q)]
     cnt = fdf['cnt'].sum()
-    print(cnt)
+    return cnt
 
+def read_parquet(path="~/tmp/history.parquet"):
+    df = pd.read_parquet(path)
+    return df
+
+
+def query():
+    q = sys.argv[1]
+    i = cnt(q)
+    print(f'질의:{q}에 대한 결과는 {i}입니다')
+    print('질의:%s에 대한 결과는 %d입니다' %(q,i))
+    print('질의:%10s에 대한 결과는 %d입니다' %(q,i))
+    print('질의:%10s에 대한 결과는 %d입니다' %(q,i))
